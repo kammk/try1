7ef1033fe07eda33140e8b64be9e08ffbaad3d78
kammk <53467992+kammk@users.noreply.github.com>
2020-09-21 15:26:56 +0200

    Add files via upload

--- codenew.m
+++ codenew.m
@@ -1,10 +1,12 @@
 clear
 a=10;
 b=20;
+
+% comments
 c=0;
 for i=1:a
-for j=1:b
-c=c+i+j; # here is need more attention
-end
+    for j=1:b
+        c=c+i+j; % here is need more attention
+    end
 end
 c
